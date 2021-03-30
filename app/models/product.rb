8d8a50097daeb30451042b856263dd718a5498ba
zyzzyxis <13ezimm@gmail.com>
2021-03-30 15:31:49 -0600

    product model sql added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :seller
   serialize :category, Array
+
+ def self.available
+  select('p.id AS product_id, p.price, p.description, p.category, s.id AS seller_id, s.name, s.email')
+  .from('products as p')
+  .joins("INNER JOIN sellers AS s ON s.id = p.seller_id")
+  .order('s.name')
+ end
 end
